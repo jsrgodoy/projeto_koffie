780d87011c371d41030bb4d4c469ca0e8735c089
Danillo.sdd <danillo.sdd@hotmail.com>
2023-10-24 14:49:13 -0300

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,6 @@
+
+/*------ LOGIN -----*/
+
 function login() {
 
   const login = document.querySelector(".login-form");
@@ -8,4 +11,30 @@ function login() {
     login.style.visibility = "hidden";
   }
 
+}
+
+/*------ VOLTAR AO TOPO -----*/
+
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
