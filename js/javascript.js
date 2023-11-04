495e26ec1d42bef97f41dd163da3aa6995174253
diegodevweb <diego.devwebb@gmail.com>
2023-11-04 17:04:15 -0300

    refactor:Capturar quantidade, calcular e mostrar no console.log()

--- js/javascript.js
+++ js/javascript.js
@@ -38,3 +38,39 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Pegando a quantidade, texto span e valor dos produtos, depois calculando e exibindo no console.log()
+
+var pedidos = [];
+var totalGeral = 0;
+
+document.querySelectorAll(".buy-btn").forEach(function(btn) {
+  btn.addEventListener("click", function(event) {
+    event.preventDefault(); // impede a ação padrão do "a href"
+
+    var product = event.target.closest('.wsk-cp-product');
+
+    var nome = product.querySelector(".title-product h3").innerText;
+    var quantidade = parseInt(product.querySelector(".quantidade").value);
+    
+    // converte a string de preço em um número
+    var preco = parseFloat(product.querySelector(".price").innerText.replace("R$ ", "").replace(",", "."));
+
+    var total = quantidade * preco;
+
+    
+
+    pedidos.push({nome: nome, quantidade: quantidade, total: total});
+
+    totalGeral += total;
+
+    console.log(nome + " - Quantidade (" + quantidade + ") - Total (R$ " + total.toFixed(2).replace(".", ",") + ")");
+
+    console.log("Total Geral: R$ " + totalGeral.toFixed(2).replace(".", ","));
+
+  });
+
+ 
+});
+
+
