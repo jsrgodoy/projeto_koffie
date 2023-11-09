4d5a5c4a690a5239bd3af92ddc8f10475ad26ec9
diegodevweb <diego.devwebb@gmail.com>
2023-11-09 11:27:01 -0300

    feat/pedidos: Capturando produtos, calculando e exibindo na tela de pedido

---
+++ js/pedido.js
@@ -0,0 +1,43 @@
+// Recuperando os valores da página produtos
+
+let totalPedidos = JSON.parse(sessionStorage.getItem("pedidos"));
+let totalValores = parseFloat(sessionStorage.getItem("totalGeral"));
+let mostrarItens = document.getElementById("itens-pedido");
+
+if (totalPedidos && totalPedidos.length > 0) {
+    totalPedidos.forEach(function(pedido) {
+      let nome = pedido.nome;
+      let quantidade = pedido.quantidade;
+      let preco = pedido.total.toFixed(2).replace(".", ",");
+  
+      let itemPedido = document.createElement("div");
+      itemPedido.classList.add("pedido-item");
+  
+      let nomeProduto = document.createElement("p");
+      nomeProduto.textContent = nome;
+  
+      let quantidadeProduto = document.createElement("p");
+      quantidadeProduto.textContent = `Quantidade: ${quantidade}`;
+  
+      let precoProduto = document.createElement("p");
+      precoProduto.textContent = `Preço: R$ ${preco}`;
+  
+
+      itemPedido.appendChild(nomeProduto);
+      itemPedido.appendChild(quantidadeProduto);
+      itemPedido.appendChild(precoProduto);
+  
+      mostrarItens.appendChild(itemPedido);
+      
+    });
+
+    let valores = document.querySelector(".valores p");
+    let precoTotal = document.querySelector(".box2-parte2 .valores h1");
+    valores.textContent = `R$ Subtotal: ${totalValores.toFixed(2).replace(".", ",")}`;
+    precoTotal.textContent = `R$ Total: ${totalValores.toFixed(2).replace(".", ",")}`;
+
+  } else {
+    mostrarItens.textContent = "Nenhum pedido selecionado.";
+  }
+
+ 
