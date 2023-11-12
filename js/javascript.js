b98fe8eff34742f31e03f7b6a75ae0b9c27c9692
Danillo.sdd <danillo.sdd@hotmail.com>
2023-11-12 12:23:03 -0300

    update

--- js/javascript.js
+++ js/javascript.js
@@ -5,9 +5,21 @@ const login = document.querySelector(".login-form");
 
 const account = document.querySelector(".account");
 
+const UserName = document.querySelector("#UserName");
+
+let usernameHelper = document.getElementById("username-helper");
+
+const Passwod = document.querySelector("#Passwod");
+
+let senhaHelper = document.getElementById("senha-helper");
+
+
+
 document.addEventListener('mousedown', (event) => {
   if (!(login.contains(event.target)) && !(account.contains(event.target))) {
     login.style.visibility = "hidden";
+    usernameHelper.style.display = "none";
+    senhaHelper.style.display = "none";
 
   }
 })
@@ -17,21 +29,27 @@ function abre() {
     login.style.visibility = "visible";
   } else {
     login.style.visibility = "hidden";
+    usernameHelper.style.display = "none";
+    senhaHelper.style.display = "none";
   }
 
 }
 
 function fecha() {
   login.style.visibility = "hidden";
+  usernameHelper.style.display = "none";
+  senhaHelper.style.display = "none";
 }
 
-/*
-if (login.style.visibility == "hidden") {
-  login.style.visibility = "visible";
-} else {
-  login.style.visibility = "hidden";
-}
-*/
+document.querySelector('.log-btn').addEventListener('click', function (event) {
+
+  usernameHelper.innerText = 'Usuário não encontrado'
+  usernameHelper.style.display = "block";
+
+  senhaHelper.innerText = 'Senha inválida'
+  senhaHelper.style.display = "block";
+});
+
 
 /*------ VOLTAR AO TOPO -----*/
 
